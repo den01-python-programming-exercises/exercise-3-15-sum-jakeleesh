a4cb394c19a089531643816fe237c2b495b7ed36
Jake Lee <jake.lee@fdmgroup.com>
2021-10-02 22:31:13 +0800

    Attempts solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,17 @@
 def main():
     #write your code below this line
+    numbers = []
+    numbers.append(3)
+    numbers.append(2)
+    numbers.append(6)
+    numbers.append(-1)
+    print(sum_list(numbers))
+
+def sum_list(numbers):
+    sum = 0
+    for num in numbers:
+        sum += num
+    return sum
 
 if __name__ == '__main__':
     main()
